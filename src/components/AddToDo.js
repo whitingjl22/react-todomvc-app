c626d5ec754c9eb05ee30554f8510a6f60298382
WHITING, JEREMY <jeremy.whiting23@gmail.com>
2019-03-22 12:51:38 -0500

    add post

--- src/components/AddToDo.js
+++ src/components/AddToDo.js
@@ -3,7 +3,8 @@ import React, { Component } from "react"
 const AddToDo = (props) => {
   return (
     <div className="new-todo edit">
-      <input type="text" placeholder="what needs to be done?" />
+      <input onChange={props.onChange} id="todo" type="text" placeholder="what needs to be done?" />
+      <button onClick={props.submit}>Add</button>
     </div>
   )
 }
--- src/container/ToDoPage.js
+++ src/container/ToDoPage.js
@@ -2,6 +2,7 @@ import React, { Component } from "react"
 import AddToDo from "../components/AddToDo"
 import ToDoList from "../components/ToDoList"
 import ToDoFooter from "../components/ToDoFooter"
+import Axios from "axios"
 
 class ToDoPage extends React.Component {
   constructor(props) {
@@ -36,13 +37,17 @@ class ToDoPage extends React.Component {
     }
   }
 
+  onChange = (event) => {
+    this.setState({ [event.target.id]: event.target.value })
+  }
+
+  submit = () => {}
+
   filter = (statusType) => {
     let filteredList
 
     if (statusType !== "All") {
-      filteredList = this.state.listItems.filter(
-        (item) => item.status === statusType
-      )
+      filteredList = this.state.listItems.filter((item) => item.status === statusType)
     } else filteredList = this.state.listItems
     this.setState({ DisplayList: filteredList })
   }
@@ -51,12 +56,9 @@ class ToDoPage extends React.Component {
     return (
       <div>
         <h1>todos</h1>
-        <AddToDo />
+        <AddToDo onChange={this.onChange} submit={this.submit} />
         <ToDoList listItems={this.state.DisplayList} />
-        <ToDoFooter
-          itemsLeft={this.state.itemsLeft}
-          filterFunction={this.filter}
-        />
+        <ToDoFooter itemsLeft={this.state.itemsLeft} filterFunction={this.filter} />
       </div>
     )
   }
